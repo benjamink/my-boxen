0e929d439f9245194c91de4683493e0edebf94f5
Benjamin Krein <superbenk@superk.org>
2014-03-13 10:32:37 -0400

    Add Google Chrome plugin

--- manifests/site.pp
+++ manifests/site.pp
@@ -92,6 +92,9 @@ node default {
   # firefox
   include firefox
 
+  # chrome
+  include chrome
+
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
